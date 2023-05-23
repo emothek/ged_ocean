b31846d4e29d028677737ff760751ad61dea5cd9
emothek <mokhtar.developer@gmail.com>
2023-05-23 02:11:30 +0100

    feat/uploadCloud

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const {
   getRefreshToken,
 } = require("../authenticate");
 const jwt = require("jsonwebtoken");
-const { tryCatch} =require('../utils/tryCatch') 
+const { tryCatch } = require("../utils/tryCatch");
 
 const { PrismaClient, Role } = require("@prisma/client");
 // Initialize a prisma client
@@ -20,155 +20,168 @@ function exclude(user, keys) {
   return user;
 }
 
-router.post("/signup", tryCatch((req, res, next) => {
-  passport.authenticate("signup", { session: false }, (err, user, info) => {
-    // Check for errors
-    if (err) throw new Error(err);
-    // Generate token
-    const token = generateToken(user.id);
+router.post(
+  "/signup",
+  tryCatch((req, res, next) => {
+    passport.authenticate("signup", { session: false }, (err, user, info) => {
+      // Check for errors
+      if (err) throw new Error(err);
+      // Generate token
+      const token = generateToken(user.id);
 
-    if (user) {
-      res.cookie("refreshToken", user.refreshToken, COOKIE_OPTIONS);
-      const userWithEx = exclude(user, ["password", "refreshToken"]);
-      return res.status(201).json({
-        status: "success",
-        data: {
-          message: "Account created.",
-          userWithEx,
-          token,
-        },
-        statusCode: res.statusCode,
-      });
-    } else {
-      res.status(400).json({
-        status: "error",
-      });
-    }
-  })(req, res, next);
-}));
+      if (user) {
+        res.cookie("refreshToken", user.refreshToken, COOKIE_OPTIONS);
+        const userWithEx = exclude(user, ["password", "refreshToken"]);
+        return res.status(201).json({
+          status: "success",
+          data: {
+            message: "Account created.",
+            userWithEx,
+            token,
+          },
+          statusCode: res.statusCode,
+        });
+      } else {
+        res.status(400).json({
+          status: "error",
+        });
+      }
+    })(req, res, next);
+  })
+);
 
-router.post("/login", tryCatch((req, res, next) => {
-  passport.authenticate("login", { session: false }, (err, user, info) => {
-    // Check for errors
-    if (err) throw new Error(err);
-    // Generate token
-    const token = generateToken(user.id);
-    if (user) {
-      res.cookie("refreshToken", user.refreshToken, COOKIE_OPTIONS);
-      const userWithEx = exclude(user, ["password", "refreshToken"]);
-      return res.status(201).json({
-        status: "success",
-        data: {
-          message: "Welcome back.",
-          userWithEx,
-          token,
-        },
-        statusCode: res.statusCode,
-      });
-    } else {
-      res.status(400).json({
-        status: "error",
-        message: info.message || "Unkown error",
-      });
-    }
-  })(req, res, next);
-}));
+router.post(
+  "/login",
+  tryCatch((req, res, next) => {
+    passport.authenticate("login", { session: false }, (err, user, info) => {
+      // Check for errors
+      if (err) throw new Error(err);
+      // Generate token
+      const token = generateToken(user.id);
+      if (user) {
+        res.cookie("refreshToken", user.refreshToken, COOKIE_OPTIONS);
+        const userWithEx = exclude(user, ["password", "refreshToken"]);
+        return res.status(201).json({
+          status: "success",
+          data: {
+            message: "Welcome back.",
+            userWithEx,
+            token,
+          },
+          statusCode: res.statusCode,
+        });
+      } else {
+        res.status(400).json({
+          status: "error",
+          message: info.message || "Unkown error",
+        });
+      }
+    })(req, res, next);
+  })
+);
 
 //refreshToken
-router.post("/refreshToken", tryCatch(async (req, res, next) => {
-  const { signedCookies = {} } = req;
+router.post(
+  "/refreshToken",
+  tryCatch(async (req, res, next) => {
+    const { signedCookies = {} } = req;
 
-  const { refreshToken } = signedCookies;
+    const { refreshToken } = signedCookies;
 
-  if (refreshToken) {
-    try {
-      const payload = jwt.verify(
-        refreshToken,
-        process.env.REFRESH_TOKEN_SECRET
-      );
+    if (refreshToken) {
+      try {
+        const payload = jwt.verify(
+          refreshToken,
+          process.env.REFRESH_TOKEN_SECRET
+        );
 
-      const userId = payload.id;
+        const userId = payload.id;
 
-      await prisma.user.findFirst({ where: { id: userId } }).then(
-        (user) => {
-          if (user) {
-            // Find the refresh token against the user record in database
+        await prisma.user.findFirst({ where: { id: userId } }).then(
+          (user) => {
+            if (user) {
+              // Find the refresh token against the user record in database
 
-            if (user.refreshToken !== refreshToken) {
+              if (user.refreshToken !== refreshToken) {
+                res.statusCode = 401;
+
+                res.send("Unauthorized,token not matched");
+              } else {
+                const token = generateToken(user.id);
+
+                // If the refresh token exists, then create new one and replace it.
+
+                const newRefreshToken = getRefreshToken({ id: userId });
+
+                user = prisma.user
+                  .update({
+                    where: {
+                      id: user.id,
+                    },
+                    data: {
+                      refreshToken: newRefreshToken,
+                    },
+                  })
+                  .then(() => {
+                    res.cookie("refreshToken", newRefreshToken, COOKIE_OPTIONS);
+
+                    res.send({ success: true, token });
+                  });
+              }
+            } else {
               res.statusCode = 401;
 
-              res.send("Unauthorized,token not matched");
-            } else {
-              const token = generateToken(user.id);
-
-              // If the refresh token exists, then create new one and replace it.
-
-              const newRefreshToken = getRefreshToken({ id: userId });
-
-              user = prisma.user
-                .update({
-                  where: {
-                    id: user.id,
-                  },
-                  data: {
-                    refreshToken: newRefreshToken,
-                  },
-                })
-                .then(() => {
-                  res.cookie("refreshToken", newRefreshToken, COOKIE_OPTIONS);
-
-                  res.send({ success: true, token });
-                });
+              res.send("Unauthorized, no user");
             }
-          } else {
-            res.statusCode = 401;
+          },
 
-            res.send("Unauthorized, no user");
-          }
-        },
+          (err) => next(err)
+        );
+      } catch (err) {
+        res.statusCode = 401;
 
-        (err) => next(err)
-      );
-    } catch (err) {
+        res.send("Unauthorized,no rt");
+      }
+    } else {
       res.statusCode = 401;
 
-      res.send("Unauthorized,no rt");
+      res.send("Unauthorized,no token");
     }
-  } else {
-    res.statusCode = 401;
-
-    res.send("Unauthorized,no token");
-  }
-}));
+  })
+);
 //logout
 
-router.get("/logout", auth, tryCatch(async (req, res, next) => {
-  const { signedCookies = {} } = req;
+router.get(
+  "/logout",
+  auth,
+  tryCatch(async (req, res, next) => {
+    const { signedCookies = {} } = req;
 
-  const { refreshToken } = signedCookies;
+    const { refreshToken } = signedCookies;
 
-  await prisma.user.findFirst({ where: { id: req.user.id } }).then(
-    (user) => {
-      if (user.refreshToken === refreshToken) {
-        user = prisma.user
-          .update({
-            where: {
-              id: user.id,
-            },
-            data: {
-              refreshToken: "",
-            },
-          })
-          .then(() => {
-            res.clearCookie("refreshToken", COOKIE_OPTIONS);
+    await prisma.user.findFirst({ where: { id: req.user.id } }).then(
+      (user) => {
+        if (user.refreshToken === refreshToken) {
+          user = prisma.user
+            .update({
+              where: {
+                id: user.id,
+              },
+              data: {
+                refreshToken: "",
+              },
+            })
+            .then(() => {
+              res.clearCookie("refreshToken", COOKIE_OPTIONS);
 
-            res.send({ success: true });
-          });
-      }
-    },
-    (err) => next(err)
-  );
-}));
+              res.send({ success: true });
+            });
+        }
+      },
+      (err) => next(err)
+    );
+  })
+);
 
 router.get(
   "/sa/users",
@@ -193,8 +206,8 @@ router.get(
     });
 
     res.send(users);
-  }
-));
+  })
+);
 
 router.get(
   "/admin/users",
@@ -253,7 +266,6 @@ router.post(
         },
         data: {
           validByAdmin: true,
-          
         },
       })
       .then(() => {
@@ -262,114 +274,126 @@ router.post(
   })
 );
 
-router.delete("/user/:id", auth, tryCatch(async (req, res) => {
-  let id = req.params.id;
+router.delete(
+  "/user/:id",
+  auth,
+  tryCatch(async (req, res) => {
+    let id = req.params.id;
 
-  await prisma.user
-    .delete({
-      where: {
-        id: parseInt(id),
-      },
-    })
-    .then(() => {
-      res.send("done !");
-    });
-}));
+    await prisma.user
+      .delete({
+        where: {
+          id: parseInt(id),
+        },
+      })
+      .then(() => {
+        res.send("done !");
+      });
+  })
+);
 
 //user_activities
 
-router.post("/user/activity", auth, tryCatch(async (req,res)=>{
-  let id=req.user.id;
-  
-  let activity=req.body.activity;
-  if(activity === "userLoggedIn" || activity === "userLoggedOut" || activity === "userSignedUp")
-  {
-    await prisma.activity.create({
-    data:{
-      activity:activity,
-      userId:id,
-      organisationId:req.user.organisationId,
-      payload:{key:req.user.email}
-    }
-  }).then(activity => res.status(200).send(activity))
-  .catch(err =>res.status(400).send(err))
-}
-else if(activity === "userNavigate")
-{
-  await prisma.activity.create({
-    data:{
-      activity:activity,
-      userId:id,
-      organisationId:req.user.organisationId,
-      payload:{route:req.body.route}
-    }
-  }).then(activity => res.status(200).send(activity))
-  .catch(err =>res.status(400).send(err))
-}
-else{
-  if (Array.isArray(req.body.doc_id)) {
-    req.body.doc_id.forEach(did => {
-      prisma.activity.create({
-        data:{
-          activity:activity,
-          userId:id,
-          organisationId:req.user.organisationId,
-          
-          payload:{key:did}
-        }
-      }).then(activity => console.log(activity))
-      .catch(err =>console.log(err))
-    });
-    return res.send('done');
-  }
-  else
-  {await prisma.activity.create({
-    data:{
-      activity:activity,
-      userId:id,
-      organisationId:req.user.organisationId,
-      fileId:req.body.doc_id,
-      payload:{key:req.body.doc_id}
-    }
-  }).then(activity => res.status(200).send(activity))
-  .catch(err =>res.status(400).send(err))}
-}
-}
+router.post(
+  "/user/activity",
+  auth,
+  tryCatch(async (req, res) => {
+    let id = req.user.id;
 
-))
+    let activity = req.body.activity;
+    if (
+      activity === "userLoggedIn" ||
+      activity === "userLoggedOut" ||
+      activity === "userSignedUp"
+    ) {
+      await prisma.activity
+        .create({
+          data: {
+            activity: activity,
+            userId: id,
+            organisationId: req.user.organisationId,
+            payload: { key: req.user.email },
+          },
+        })
+        .then((activity) => res.status(200).send(activity))
+        .catch((err) => res.status(400).send(err));
+    } else if (activity === "userNavigate") {
+      await prisma.activity
+        .create({
+          data: {
+            activity: activity,
+            userId: id,
+            organisationId: req.user.organisationId,
+            payload: { route: req.body.route },
+          },
+        })
+        .then((activity) => res.status(200).send(activity))
+        .catch((err) => res.status(400).send(err));
+    } else {
+      if (Array.isArray(req.body.doc_id)) {
+        req.body.doc_id.forEach((did) => {
+          prisma.activity
+            .create({
+              data: {
+                activity: activity,
+                userId: id,
+                organisationId: req.user.organisationId,
 
-router.get("/activities",auth, tryCatch(async (req,res)=>{
-  let user =req.user;
-  if(user.role === "SUPERADMIN"){
-
-    await prisma.activity.findMany({include:{user:true,file:true}}).then(activities => 
-     
-      res.status(200).send(activities)
-    
-    )
-    .catch(err =>res.status(400).send(err))
-  }
-  else if(user.role === "ADMIN"){
-
-    await prisma.activity.findMany(
-      {
-        where:{
-          organisationId:req.user.organisationId
-        },
-        include:{user:true,file:true}
+                payload: { key: did },
+              },
+            })
+            .then((activity) => console.log(activity))
+            .catch((err) => console.log(err));
+        });
+        return res.send("done");
+      } else {
+        await prisma.activity
+          .create({
+            data: {
+              activity: activity,
+              userId: id,
+              organisationId: req.user.organisationId,
+              fileId: req.body.doc_id,
+              payload: { key: req.body.doc_id },
+            },
+          })
+          .then((activity) => res.status(200).send(activity))
+          .catch((err) => res.status(400).send(err));
       }
-    ).then(activities => 
-     
-      res.status(200).send(activities)
-    
-    )
-    .catch(err =>res.status(400).send(err))
-  }
-  
-}))
+    }
+  })
+);
 
-router.get("/profile", auth, tryCatch((req, res, next) => {
-  return res.json({ message: "Welcome friend", user: req.user });
-}));
+router.get(
+  "/activities",
+  auth,
+  tryCatch(async (req, res) => {
+    let user = req.user;
+    if (user.role === "SUPERADMIN") {
+      await prisma.activity
+        .findMany({ include: { user: true, file: true } })
+        .then((activities) => res.status(200).send(activities))
+        .catch((err) => res.status(400).send(err));
+    } else if (user.role === "ADMIN") {
+      await prisma.activity
+        .findMany({
+          where: {
+            organisationId: req.user.organisationId,
+          },
+          include: { user: true, file: true },
+        })
+        .then((activities) => res.status(200).send(activities))
+        .catch((err) => res.status(400).send(err));
+    }
+  })
+);
+
+router.get(
+  "/profile",
+  auth,
+  tryCatch((req, res, next) => {
+    return res.json({ message: "Welcome friend", user: req.user });
+  })
+);
 
 module.exports = router;
